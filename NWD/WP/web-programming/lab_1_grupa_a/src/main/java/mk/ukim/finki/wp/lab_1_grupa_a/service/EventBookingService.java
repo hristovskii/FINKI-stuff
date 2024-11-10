f732d37fd8114c4f0c75e7956fab93ad835c8a29
Petar Hristovski <petarhristovski23@gmail.com>
2024-11-10 19:12:22 +0100

    fixing

--- NWD/WP/web-programming/lab_1_grupa_a/src/main/java/mk/ukim/finki/wp/lab_1_grupa_a/service/EventBookingService.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.wp.lab_1_grupa_a.service;
-
-import mk.ukim.finki.wp.lab_1_grupa_a.model.EventBooking;
-
-public interface EventBookingService {
-    EventBooking placeBooking(String eventName, String attendeeName, String attendeeAddress, int numberOfTickets);
-
-}
